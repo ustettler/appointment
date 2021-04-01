60577684e7896132459b1d66d67c4089f284d9db
Urs Stettler <62382591+ustettler@users.noreply.github.com>
2021-04-01 16:07:53 +0200

    Add files via upload
    extends code

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 console.log("Kalender")
+
+let days = document.querySelector(".days");
+let currentDayEl;
+let selectedDay = -1;
+
+for(let i=1;i<32;i++){
+ const listElementLi = document.createElement("li");
+ listElementLi.innerHTML = i
+ days.appendChild(listElementLi)
+ listElementLi.addEventListener("click",() => {
+    if (currentDayEl){
+    currentDayEl.classList.remove("active");
+    }
+    setDay(i)
+    selectedDay = i;
+    listElementLi.classList.add("active")
+    currentDayEl = listElementLi
+
+ })
+}
+
+
+function setDay(number){
+    console.log(number)
+    let currentDate = document.querySelector("#currentDate");
+    currentDate.innerHTML = number + " März 2021";
+
+}
+
+let nameInput = document.querySelector("#name");
+let name = "";
+
+nameInput.addEventListener("input", (value) => {
+    name = value;
+    console.log(name)
+})
